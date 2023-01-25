caff67a35df626c5decf0e712e86ee935a29a28a
QrubyLover <QrubyLover@gmail.com>
2023-01-25 21:36:10 +0300

    HTML added

--- Ruby/6.getters & setters.rb
+++ Ruby/6.getters & setters.rb
@@ -5,12 +5,20 @@ class GettersSetters
     # if you creat an object of this class will through error. Define a func that print the name to avoid it.
   end
 
+    #create a getter method
   def name
     puts @name
   end
 
+   #create a setter method, using equal sign
+  def name=(name)
+    @name=name
+  end
 
 end
 
 ob1= GettersSetters.new("Trilp y")
+#ob1.name
+#change the  name value using setter
+ob1.name="kkk"
 ob1.name
